2e20a3ee7a6876209b9978f00f1c4243672aab54
Timothy Ofie <Boxerbuzz559@gmail.com>
2024-11-18 20:10:32 +0100

    chore: init repo

---
+++ lib/app/view/app.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:waze_kibris/counter/counter.dart';
+import 'package:waze_kibris/l10n/l10n.dart';
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        appBarTheme: AppBarTheme(
+          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        ),
+        useMaterial3: true,
+      ),
+      localizationsDelegates: AppLocalizations.localizationsDelegates,
+      supportedLocales: AppLocalizations.supportedLocales,
+      home: const CounterPage(),
+    );
+  }
+}
---
+++ lib/main_staging.dart
@@ -0,0 +1,6 @@
+import 'package:waze_kibris/app/app.dart';
+import 'package:waze_kibris/bootstrap.dart';
+
+void main() {
+  bootstrap(() => const App());
+}
