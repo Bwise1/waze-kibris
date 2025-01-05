67834e2aa223301770cfa210f8dd74485be59a5b
Esquire Jahswill <meetjahwill@gmail.com>
2025-01-05 01:14:11 +0100

    task: app router fix

--- lib/core/app_router.dart
+++ lib/core/app_router.dart
@@ -2,22 +2,23 @@ import 'package:flutter/material.dart';
 import 'package:waze_kibris/app/auth/auth.dart';
 import 'package:waze_kibris/app/dashboard/view/main_dashboard.dart';
 import 'package:waze_kibris/app/profile/profile.dart';
- import 'package:waze_kibris/common.dart';
+import 'package:waze_kibris/common.dart';
 
 class ScreenPaths {
   static String home = '/';
-  static String getStarted = '/getStarted';
-  static String verifyEmail = '/verifyEmail';
-  static String signIn = '/signIn';
-  static String dashBoard = '/dashBoard';
-  static String personalInformation = '/personalInformation';
-  static String updateUserName = '/updateUserName';
-  static String soundSettings = '/soundSettings';
-  static String helpAndFeedback= '/helpAndFeedback';
-  static String updateUsername= '/updateUsername';
-  static String addLocation= '/addLocation';
-  static String aboutUs= '/aboutUs';
-  static String deleteAccount= '/deleteAccount';
+  static String getStarted = '/get-started';
+  static String verifyEmail = '/verify-email';
+  static String signIn = '/sign-in';
+  static String dashBoard = '/dashboard';
+
+  static String personalInformation = '/personal-information';
+  static String updateUserName = '/update-username';
+  static String soundSettings = '/sound-settings';
+  static String helpAndFeedback = '/help-and-feedback';
+  static String updateUsername = '/update-username';
+  static String addLocation = '/add-location';
+  static String aboutUs = '/about-us';
+  static String deleteAccount = '/delete-account';
 }
 
 final navigatorKey = GlobalKey<NavigatorState>();
@@ -29,73 +30,66 @@ final appRouter = GoRouter(
       ScreenPaths.home,
       (state) => const OnboardScreen(),
       name: 'home',
-    )
-    ,RouteWrapper(
+    ),
+    RouteWrapper(
       ScreenPaths.getStarted,
       (state) => const GettingStarted(),
-      name: 'getStarted',
+      name: 'get-started',
     ),
-
     RouteWrapper(
       ScreenPaths.verifyEmail,
       (state) => const EmailVerification(),
-      name: 'verifyEmail',
+      name: 'verify-email',
     ),
-
     RouteWrapper(
       ScreenPaths.signIn,
       (state) => const SignInScreen(),
-      name: 'signIn',
+      name: 'sign-in',
     ),
-
     RouteWrapper(
       ScreenPaths.dashBoard,
       (state) => const MainDashboard(),
-      name: 'dashBoard',
+      name: 'dashboard',
     ),
-
     RouteWrapper(
       ScreenPaths.personalInformation,
       (state) => const PersonalInformationScreen(),
-      name: 'personalInformation',
+      name: 'personal-information',
     ),
-
     RouteWrapper(
       ScreenPaths.updateUserName,
       (state) => const UpdateUsernameScreen(),
-      name: 'updateUserName',
+      name: 'update-username',
     ),
-
     RouteWrapper(
       ScreenPaths.soundSettings,
       (state) => const SoundSettingsScreen(),
-      name: 'soundSettings',
-    )
-    ,RouteWrapper(
+      name: 'sound-settings',
+    ),
+    RouteWrapper(
       ScreenPaths.helpAndFeedback,
       (state) => const HelpAndFeedbackScreen(),
-      name: 'helpAndFeedback',
-    ),RouteWrapper(
+      name: 'help-and-feedback',
+    ),
+    RouteWrapper(
       ScreenPaths.updateUsername,
       (state) => const UpdateUsernameScreen(),
-      name: 'updateUsername',
-    )
-    ,RouteWrapper(
+      name: 'update-username',
+    ),
+    RouteWrapper(
       ScreenPaths.addLocation,
       (state) => const AddLocationScreen(),
-      name: 'addLocation',
+      name: 'add-location',
     ),
-
     RouteWrapper(
       ScreenPaths.aboutUs,
       (state) => const AboutUsScreen(),
-      name: 'aboutUs',
-    )
-
-    ,RouteWrapper(
+      name: 'about-us',
+    ),
+    RouteWrapper(
       ScreenPaths.deleteAccount,
       (state) => const DeleteAccountScreen(),
-      name: 'deleteAccount',
+      name: 'delete-account',
     ),
   ],
 );
